00a293d1e1e0a087db84d27b9ef3a5009b63c3b4
SouravSouru <souravkk2021@gmail.com>
2023-01-24 09:54:59 +0530

    apply llc

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         // useMaterial3: true
 
       ),
-      home: id == "null" ? ProfileScreen():HomeScreen(),
+      home: id == "null" ? LoginScreen():HomeScreen(),
     );
   }
 }
